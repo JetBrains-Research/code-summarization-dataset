3f7eb2736953f55c86409bdf7b8ef79154fc09a9
Vancho <isrybin@gmail.com>
2020-11-29 13:36:06 +0300

    change main dir

--- src/main/kotlin/reposfinder/main.kt
+++ src/main/kotlin/reposfinder/main.kt
@@ -5,7 +5,7 @@ fun main() {
     println("Please, input path to .json config file: ")
     val configPath = readLine()
     if (configPath != null) {
-        val finder = ReposFinder(Config(configPath), isDebug = true)
+        val finder = ReposFinder(Config(configPath, isDebug = true))
         finder.run()
     }
 }
