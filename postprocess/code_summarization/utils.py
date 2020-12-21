80f4d6f293935471288e0fd40d2bc0056d90b75a
Vancho <isrybin@gmail.com>
2020-12-21 16:05:55 +0300

    simple analysis postprocess

--- postprocess/code_summarization/utils.py
+++ postprocess/code_summarization/utils.py
@@ -1,5 +1,7 @@
 import json
 import os
+from shutil import rmtree
+
 
 def read_file(filepath):
     lines = []
@@ -17,3 +19,21 @@ def json_dump(content, filepath):
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
+
+
+def remove_if_exists(path):
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            rmtree(path)
+        elif os.path.isfile(path):
+            os.remove(path)
+
+
+def get_kb_file_size(path):
+    if os.path.exists(path):
+        size = os.path.getsize(path)
+        if size % 1024 == 0:
+            return os.path.getsize(path) / 1024
+        return int((os.path.getsize(path) + 1024) / 1024)
+
+    return 0
--- run_postprocess.sh
+++ run_postprocess.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python postprocess/main.py PATH_TO_REPO_ANALYSIS_RESULT
+python postprocess/main.py repos/analysis_results
